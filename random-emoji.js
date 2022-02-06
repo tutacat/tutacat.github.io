79a3a8e1ab1a5109edaad9ddc6f7ae974aa3380e
tutacat <fhwbml@gmail.com>
2022-02-06 04:14:41 +0000

    add stuff to stuff

--- random-emoji.js
+++ random-emoji.js
@@ -1,5 +1,5 @@
 setTimeout(function(){
-        for (elem of document.getElementsByClassName("random-emoji")) {
-          elem.innerText=String.fromCharCode(Math.floor(Math.random()*3094)+0x231a)
+        for (elem of document.querySelectorAll(".random-emoji")) {
+          elem.onclick=(e)=>{navigator.clipboard.writeText(e.target.innerText);e.target.value=e.target.innerText=String.fromCharCode(Math.floor(Math.random()*3094)+0x231a)}
         }
-      }, 1000)
+      }, 0)
