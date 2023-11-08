df86282e3798a749583139f706308e09f84a8ebf
tutacat <fhwbml@gmail.com>
2023-11-08 18:04:32 +0000

    Update random-emoji.js [fix]

--- random-emoji.js
+++ random-emoji.js
@@ -3,11 +3,11 @@ setTimeout(() => {
   for (elem of document.querySelectorAll(".random-emoji")) {
       elem.onclick = (e) => {
           setTimeout(() => {
-              navigator.clipboard.writeText(e.target.innerText);
               e.target.style.fontSize = "initial";
               e.target.value = e.target.innerText = String.fromCharCode(
                   Math.floor(Math.random()*3094)+0x231a
                 );
+              navigator.clipboard.writeText(e.target.innerText);
           },100);
           e.target.style.fontSize = "1.5em";
           e.target.innerText = "*boop*";
