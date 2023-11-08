2ad783fdd11171d7def743db7fde00684cabf1b9
tutacat <fhwbml@gmail.com>
2023-11-08 18:06:07 +0000

    Update random-emoji.js

--- random-emoji.js
+++ random-emoji.js
@@ -1,15 +1,16 @@
 /* random-emoji.js */
 setTimeout(() => {
   for (elem of document.querySelectorAll(".random-emoji")) {
-      elem.onclick = (e) => {
-          setTimeout(() => {
-              e.target.style.fontSize = "initial";e.target.innerText = String.fromCharCode(
-                  Math.floor(Math.random()*3094)+0x231a
-                );
-              navigator.clipboard.writeText(e.target.innerText);
-          },100);
-          e.target.style.fontSize = "1.5em";
-          e.target.innerText = "*boop*";
-        }
+    elem.onclick = (e) => {
+      setTimeout(() => {
+        e.target.style.fontSize = "initial";
+        e.target.innerText = String.fromCharCode(
+          Math.floor(Math.random()*3094)+0x231a
+        );
+        navigator.clipboard.writeText(e.target.innerText);
+      },100);
+      e.target.style.fontSize = "1.5em";
+      e.target.innerText = "*boop*";
     }
+  }
 }, 0);
